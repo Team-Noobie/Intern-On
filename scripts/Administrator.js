17f48ec53dabb296f5af4e9b11106f6ea9fe6079
Adace <sequena.alex@gmail.com>
2017-03-24 15:29:16 -0700

    Adjustments

--- scripts/Administrator.js
+++ scripts/Administrator.js
@@ -10,61 +10,45 @@
         $scope.init = function () {
 				$state.go('user_administrator.administrator_module');
 		};
+        $http({method: 'GET', url: urls.API_HOST + '/company_accounts_list'}).then(function(response){
+            $scope.company = response.data;
+        });
 
-          $scope.newCompanyAccount = function(id){
+        $http({method: 'GET', url: urls.API_HOST + '/coordinator_accounts_list'}).then(function(response){
+            $scope.coordinator = response.data;
+        });
+        
+
+        $scope.newCompanyAccount = function(){
                 var modalInstance = $uibModal.open({
                     animation: true,
                     templateUrl: 'create_company_accounts.html',
                     controller: 'create_company_account_controller',
                     size: 'md',
-                    resolve: {
-                            id: function () {
-                                return id;
-                            }
-                        }
                     });
 
-                    modalInstance.result.then(function (id) {
+                    modalInstance.result.then(function () {
                         return 1;
                     });
             };
-            // if($state.current.name == 'user_administrator.administrator_module'){
-            //     $scope.choice = {
-            //     'option': {'Account Type': '','value':''},
-            //                      };
-            
-            //     $scope.choices = [
-            //         {'name': 'Coordinator','value':'coordinator'},
-            //         {'name': 'Company','value':'company'},
-            //     ];
 
-                $http({method: 'GET', url: urls.API_HOST + '/company_accounts_list'}).then(function(response){
-                    $scope.company = response.data;
-                });
-            //  };
-
-        $scope.newCoordinatorAccount = function(id){
+    
+        $scope.newCoordinatorAccount = function(){
                 var modalInstance = $uibModal.open({
                     animation: true,
                     templateUrl: 'create_coordinator_accounts.html',
                     controller: 'create_coordinator_account_controller',
                     size: 'md',
-                    resolve: {
-                            id: function () {
-                                return id;
-                            }
-                        }
                     });
 
-                    modalInstance.result.then(function (id) {
+                    modalInstance.result.then(function () {
                         return 1;
                     });
             };         
 
-    });
+        });
 
         
-
      internon.controller('create_company_account_controller',function(urls,$http,$auth,$state,$scope,$localStorage,$uibModal,$uibModalInstance){
         $scope.create_company_account = function () {
             $http.post(urls.API_HOST + '/create_company_account', $scope.formdata).then(function (response){
